b4b79b57990e34c781255c43bf7bfa2723591a2c
mirrrrrrrrrrrr <mirrrrrr@yahoo.com>
2025-04-25 11:55:56 +0500

    cli completed

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
     // Write your code here 
+    const args = process.argv.slice(2);
+    console.log(args)
+    for (let i = 0; i < args.length; i += 2) {
+        const key = args[i].replace('--', '');
+        const value = args[i + 1];
+        console.log(`${key} is ${value}`);
+    }
 };
 
 parseArgs();
