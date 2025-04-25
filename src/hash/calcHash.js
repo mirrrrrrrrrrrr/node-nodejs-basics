c1fa4ed98a29eeb146016eb76ae31c18f08c3345
mirrrrrrrrrrrr <mirrrrrr@yahoo.com>
2025-04-25 13:04:32 +0500

    hash completed

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+const { createHmac } = await import("node:crypto");
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const secret = "abcdefg";
+    try {
+        const hash = createHmac("sha256", secret)
+            .update("I love cupcakes")
+            .digest("hex");
+        console.log(hash);
+    } catch (error) {
+        throw error;
+    }
 };
 
 await calculateHash();
