2cf05198b76a3f70d6fa05c1324f9edca74b6b78
mirrrrrrrrrrrr <mirrrrrr@yahoo.com>
2025-04-28 12:12:38 +0500

    cp completed

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { spawn } from "child_process";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = resolve(__filename, "..");
+    const scriptFile = resolve(__dirname, "files", "script.js");
+
+    const child = spawn("node", [scriptFile, ...args], {
+        stdio: ["pipe", "pipe", "inherit"],
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2"]);
